ad056bd0d7ca0864b5e6435aed7a7b12ff047181
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-14 23:45:39 +0200

    feat: navigation, operation fails

--- index.js
+++ index.js
@@ -1,51 +1,30 @@
-import * as fs from 'node:fs/promises';
-import * as os from 'node:os';
+
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-import { exitGracefully, displayCurrentDirectory, welcome } from './src/handlers/general.js';
-
-const rl = readline.createInterface({ input, output });
+import { promptUser } from './src/handlers/general.js';
 
 const args = process.argv.slice(2);
 const usernameArg = args.find(arg => arg.startsWith('--username='));
-const username = usernameArg ? usernameArg.split('=')[1] : 'Anonymous';
+export const name = usernameArg ? usernameArg.split('=')[1] : 'Anonymous';
 
-const homeDirectory = os.homedir();
-let currentDirectory = homeDirectory;
+export const rl = readline.createInterface({ input, output });
 
-const handleInput = async (input) => {
-  input = input.trim();
-  switch (input) {
-    case '.exit':
-      exitGracefully(rl);
-      break;
-    case 'pwd':
-      console.log(currentDirectory);
-      break;
-    case 'ls':
-      try {
-        const files = await fs.readdir(currentDirectory);
-        console.log(files.join('\n'));
-      } catch (err) {
-        console.error('Error reading directory:', err);
-      }
-      break;
-    default:
-      console.log('Unknown command. Available commands: .exit, pwd, ls');
-  }
-  promptUser();
+export const exitGracefully = () => {
+  rl.close();
+  process.exit(0);
 };
-
-const promptUser = async () => {
-  displayCurrentDirectory(currentDirectory);
-  const input = await rl.question('> ');
-  await handleInput(input);
-};
-
-welcome(username);
-promptUser();
 
 rl.on('close', () => {
-  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-  exitGracefully(rl);
+  console.log(`Thank you for using File Manager, ${name}, goodbye!`);
+  exitGracefully();
 });
+
+
+const welcome = () => {
+  console.log(`Welcome to the File Manager, ${name}!`);
+}
+
+welcome();
+promptUser();
+
+
--- src/handlers/general.js
+++ src/handlers/general.js
@@ -1,10 +1,45 @@
-export const exitGracefully = (rl) => {
-  rl.close();
-  process.exit(0);
+import * as os from 'node:os';
+import { rl, exitGracefully } from '../../index.js';
+import { navigationHandler } from './navigation.js';
+import { invalidInput, unexpectedError } from '../utils/messages.js';
+
+
+const homeDirectory = os.homedir();
+process.chdir(homeDirectory);
+
+const displayCurrentDirectory = () => {
+  console.log(`You are currently in ${process.cwd()}`);
 };
 
-export const displayCurrentDirectory = (currentDirectory) => {
-  console.log(`You are currently in ${currentDirectory}`);
+const handleInput = async (input) => {
+  input = input.trim();
+  const currentDirectory = process.cwd();
+  const [command, ...params] = input.trim().split(' ');
+  try {
+    switch (command) {
+      case '.exit':
+        exitGracefully();
+        break;
+      case 'pwd':
+        console.log(currentDirectory);
+        break;
+      case 'up':
+      case 'cd':
+      case 'ls':
+        navigationHandler(command, params);
+        break;
+      default:
+        invalidInput(command);
+    }
+  } catch (err) {
+    unexpectedError(input, err);
+
+  }
+  promptUser();
 };
 
-export const welcome = (username) => console.log(`Welcome to the File Manager, ${username}!`);
+export const promptUser = async () => {
+  displayCurrentDirectory(process.cwd());
+  const input = await rl.question('> ');
+  await handleInput(input);
+};
