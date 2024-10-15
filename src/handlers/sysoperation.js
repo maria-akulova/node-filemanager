34c930127fb2b2912c0e2344062ec9028a846697
maria.akulova <maria.akulova.dragon@gmail.com>
2024-10-15 02:41:37 +0200

    feat: os cpus

--- src/handlers/sysoperation.js
+++ src/handlers/sysoperation.js
@@ -2,10 +2,17 @@ import { readFile, writeFile, rename, rm } from 'node:fs/promises';
 import { pipeline } from 'node:stream/promises';
 import { createReadStream, createWriteStream } from 'node:fs';
 import { join, resolve } from 'node:path';
-import { homedir, userInfo, cpus, EOL, arch } from 'node:os';
+import { homedir, userInfo, availableParallelism, cpus, EOL, arch } from 'node:os';
 
 const printEOL = () => {
   console.log(`default system End-Of-Line is ${JSON.stringify(EOL)}`);
+}
+
+const hostCpusInfo = () => {
+  console.log(`Amount of CPUs is ${availableParallelism()}`);
+  const allCpus = cpus();
+  allCpus.map((cpu, index) => console.log(`${index} model: ${cpu.model}, clock rate: ${(cpu.speed / 1000).toFixed(2)} GHz`))
+
 }
 
 export const sysOperations = (params) => {
@@ -21,7 +28,7 @@ export const sysOperations = (params) => {
           printEOL();
           break;
         case 'cpus':
-          start(getFilePath(params));
+          hostCpusInfo();
           break;
         case 'homedir':
           start(params);
